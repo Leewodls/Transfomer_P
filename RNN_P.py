a0bc53c38ff3fa60d034c43d316c6d6ceff5f3bb
Leewodls <wodls2436@gmail.com>
2025-02-16 01:06:23 +0900

    RNN 실습 코드작성

---
+++ RNN_P.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class MyRNN(nn.module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size +hidden_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = torch.tanh(self.i2h(combined))
+        output = self.h2o(hidden)
+        return output, hidden
+    
+    def get_hidden(self):
+        return torch.zeros(1, self.hidden_size)
+    
+rnn_model = MyRNN(input_size=4, hidden_size=1024, output_size=2)
+hidden = rnn_model.get_hidden()
+
+_, hidden = rnn_model(input_tesor0, hidden)
+_, hidden = rnn_model(input_tesor1, hidden)
+_, hidden = rnn_model(input_tesor2, hidden)
+output_tensor3, _ = rnn_model(input_tesor3, hidden)
